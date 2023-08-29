e57e17572efd331033067d6c9183cc72e6c7fc17
Jeanne <j.pietropaoli@yahoo.fr>
2023-08-29 20:20:03 +0200

    Create empty component for code structure

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+    return (
+        <div className="home">
+            <h1 id="Home">Jeanne Pietropaoli</h1>
+            <h2>Front-End Web Developper</h2>
+        </div>
+    )
+}
